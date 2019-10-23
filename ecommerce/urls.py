4037c810231afc953b278bed0dfdfb2b9c8565b4
Ubuntu <ubuntu@ip-172-31-36-9.ec2.internal>
2019-10-23 11:31:04 +0000

    css style sheet updated

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,9 +23,10 @@ from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$/', all_products, name='index'),
+    url(r'^$', all_products, name='index'),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
