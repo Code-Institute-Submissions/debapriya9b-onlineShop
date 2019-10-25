874e4e2be3ba1e8a48b11a6fd4a5e54cc1161ba6
Ubuntu <ubuntu@ip-172-31-36-9.ec2.internal>
2019-10-25 11:05:50 +0000

    settings.py modified with if logic in env import for travis to work

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+if os.path.exists('env.py'):
+  import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
