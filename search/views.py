c3a0268f784762e315831b79455c2c9996b07b50
Ubuntu <ubuntu@ip-172-31-36-9.ec2.internal>
2019-11-03 17:52:13 +0000

    pagination added in search view

--- search/views.py
+++ search/views.py
@@ -1,7 +1,17 @@
+from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from products.models import Product
 
 # Create your views here.
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
+    paginator = Paginator(products, 3) # Show 3 products per page
+    
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     return render(request, "products.html", {"products": products})
