177879e60aef2f27b0f20f72c4a1ca381fb71bf9
Ubuntu <ubuntu@ip-172-31-36-9.ec2.internal>
2019-11-03 23:53:29 +0000

    Product model changed,category field added to the model

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
+    category = models.CharField(max_length=50, default='')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
 
