f9e801de6847c3a66f40c7dfa9144baeac4ee484
Ubuntu <ubuntu@ip-172-31-36-9.ec2.internal>
2019-11-04 12:52:09 +0000

    ecommerce site connected to email in settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -170,4 +170,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
+
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
