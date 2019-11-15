0c5645fb4ea50f744896bcdba9757b71f3619a1a
Ubuntu <ubuntu@ip-172-31-36-9.ec2.internal>
2019-11-15 11:59:07 +0000

    Delete option added for posts

--- posts/views.py
+++ posts/views.py
@@ -44,3 +44,18 @@ def create_or_edit_post(request, pk=None):
     else:
         form = BlogPostForm(instance=post)
     return render(request, 'blogpostform.html', {'form': form})
+    
+    
+def post_delete(request, pk):    
+    """
+    To delete a post
+    """
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+        ).order_by('-published_date')
+    return render(request, "blogposts.html", {'posts': posts})
+    
+    
+    
+    
