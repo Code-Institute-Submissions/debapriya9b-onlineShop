9dda1eb13f889242c9ab9b38997b7d3b9ddcf0c5
Ubuntu <ubuntu@ip-172-31-36-9.ec2.internal>
2019-11-19 16:40:50 +0000

    models.py modified under post by adding another field named author

--- posts/forms.py
+++ posts/forms.py
@@ -6,4 +6,4 @@ class BlogPostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'content', 'image', 'tag', 'published_date')
+        fields = ('title','author', 'content', 'image', 'tag', 'published_date')
--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     A single Blog post
     """
     title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
