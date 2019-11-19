996e87ec0efbbf75411b77fea8e1dc4e9695a6eb
Ubuntu <ubuntu@ip-172-31-36-9.ec2.internal>
2019-11-19 17:33:50 +0000

    delete view modified for post

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import BlogPostForm
+#from django.contrib import messages
 
 
 def get_posts(request):
@@ -55,7 +56,6 @@ def post_delete(request, pk):
     posts = Post.objects.filter(published_date__lte=timezone.now()
         ).order_by('-published_date')
     return render(request, "blogposts.html", {'posts': posts})
-    
     
     
     
